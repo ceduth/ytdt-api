fc88b548192c9adaebc559088184f4290bea4098
Edouard C <carvalho.edouard.pc@jesusfilm.org>
2024-11-13 21:42:12 +0000

    ✨ add get/test video scripts

---
+++ get_videos.py
@@ -0,0 +1,115 @@
+"""
+Extract metadata from YouTube videos 
+using the [google-api-python-client](https://googleapis.github.io/google-api-python-client/)
+Based on YouTube Data API (v3) 
+
+"""
+
+import pandas as pd 
+import logging
+from glom import glom
+from dataclasses import dataclass, asdict, fields
+from googleapiclient.discovery import build
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+@dataclass
+class Video:
+  id: str
+  title: str
+  published_at: str
+  language_code: str
+  thumbnail_url: str
+  duration: str
+  view_count: str
+
+  def __str__(self):
+    return f"{self.id} {self.title}"
+
+  @classmethod
+  @property
+  def fields(self):
+    return [field.name for field in fields(Video)]
+
+
+
+api_key = "AIzaSyCdx2AVAwQxFu5c69HpLQW8MQkexLXOw18"
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
+def get_video_data(video_ids):
+  """
+  Note: Retrieval may fail for some videos so output may have length < len(video_ids)
+  Cost of retrieving YT videos is 1 unit per 50 videos capped at 10k units/day.
+  https://developers.google.com/youtube/v3/determine_quota_cost
+
+  """
+  
+  num_batches = min(int(len(video_ids)/50), 10000) + 1
+  for i in range(num_batches):
+
+    # coerces video_ids into an iterable
+    if not hasattr(video_ids, '__iter__'):
+      assert isinstance(video_ids, str), f"Invalid arg 'video_ids' {type(video_ids)}"
+      video_ids = video_ids.split(',')
+
+    # YT denies more than 50 video ids per request
+    id_list = list(video_ids)[i*50: (i+1)*50]
+    request = youtube.videos().list(
+        part="snippet,contentDetails,statistics",
+        id=','.join(list(id_list)) 
+    )
+
+    # adapt response
+    response = request.execute()
+    for item in response['items']:
+      msg, video = None, None
+
+      try:
+        video = Video(
+          id=item['id'],
+          title=item['snippet']['title'],
+          published_at=item['snippet']['publishedAt'],
+          language_code=glom(item, 'snippet.defaultAudioLanguage', default=''),
+          thumbnail_url=glom(item, 'snippet.thumbnails.default.url', default=''),
+          duration=item['contentDetails']['duration'],
+          view_count=item['statistics']['viewCount'],
+        )
+
+        logging.debug(f"Extracted video : {item['id']}", asdict(video))
+        msg = None
+
+      except Exception as exc:
+        msg, video = str(exc), None
+        logging.debug(f"Error extracting video {msg}")
+
+      yield msg, video
+
+
+    
+
+if __name__ == '__main__':
+    
+  dry_run = False
+  # csv_input_path = 'data/yt-null-upload-dates.csv'
+  csv_input_path = 'data/2024-11-08 BP Wonder Series Videos for Checking.xlsx - Data.csv'
+  csv_output_path = f"{csv_input_path}-out.xlsx"
+
+  df = pd.read_csv(csv_input_path)
+  df.set_index('yt_video_id', drop=False, inplace=True)
+  df = df.reindex(columns=df.columns.tolist()+Video.fields)
+  # df.dropna(inplace=True)
+
+  # update 
+  video_ids = df['yt_video_id']
+  msgs, videos = list(zip(*get_video_data(video_ids)))
+  for video in videos:
+    df.loc[video.id] = { **df.loc[video.id].to_dict(), **asdict(video) }
+
+  # update csv file
+  if not dry_run:
+    df.to_excel(csv_output_path, engine='xlsxwriter', index=False)
+    
+    
---
+++ test_videos.py
@@ -0,0 +1,78 @@
+# import re
+# import urllib.request
+# from bs4 import BeautifulSoup as bs
+
+
+# video_id = "Y8JJi_YkXtA"
+# with urllib.request.urlopen("https://www.youtube.com/watch?v=" + video_id) as r:
+#   html = r.read()
+#   yt_html = bs(html, "html.parser")
+
+#   pattern = 'Video unavailable'
+#   data = re.search(pattern=pattern, string=yt_html.prettify())
+
+import logging
+import csv 
+from pytube import YouTube
+from pytube.exceptions import RegexMatchError, VideoUnavailable
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+def download_youtube_video(video_id, csv_output_path="videos/", dry_run=False):
+
+    try:
+
+        status, msg = -1, ""
+        yt = YouTube(f"https://www.youtube.com/watch?v={video_id}")
+        yt.check_availability()
+        status, msg = 0, f"Video is available : {yt.title}"
+
+        if not dry_run:
+            stream = yt.streams.get_highest_resolution()
+            stream.download(csv_output_path)
+            status, msg = 0, f"Successfully downloaded : {yt.title}"
+
+    except RegexMatchError:
+        status, msg = -1, "Invalid YouTube URL." 
+        # logging.debug(msg)
+    except VideoUnavailable as e:
+        status, msg =  0, "Video Unavailable"
+    except Exception as e:
+        status, msg = -1, str(e)
+    
+    return status, msg
+    
+
+
+if __name__ == '__main__':
+    
+    # yt_video_ids = { "yt_video_id": "Y8JJi_YkXtA" }
+    csv_input_path = 'yt-nullchannel.csv'
+    csv_output_path = f"{csv_input_path}-out.csv"
+    csv_header = ['yt_video_id', 'availability']
+
+
+    with open(csv_input_path, 'r', newline='') as input_csv, \
+         open(csv_output_path, 'w', newline='') as output_csv:
+
+        # print('\n\n')
+        # print("{:<20} {:<10}".format("Video ID", "Availability"))
+        # print("-"*42)
+        writer = csv.DictWriter(output_csv, fieldnames=csv_header)
+        writer.writeheader()
+
+        for row in csv.DictReader(input_csv):
+            video_id = row['yt_video_id']
+            status, msg = download_youtube_video(video_id, dry_run=True)
+            row['availability'] = msg
+            writer.writerow(row)
+            # print("{:<20} {:<10}".format(video_id, msg))
+
+
+
+
+     
+
+     
